453cb1940ded780228029268dae805b98fd4a947
lai2-24 <lai2-24@itismeucci.com>
2025-10-15 09:00:32 +0200

    mioThread

---
+++ src/main/java/com/example/MioThread.java
@@ -0,0 +1,70 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MioThread {
+
+    Socket mioSocket;
+
+    public MioThread(Socket mioSocket){
+
+        this.mioSocket = mioSocket;
+
+    }
+    public void start() throws IOException{
+       
+        BufferedReader in = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(mioSocket.getOutputStream(), true);
+        out.println("versione 1.0");
+        in.readLine();
+
+        String input;
+        while (true) {
+            input = in.readLine();  
+        
+            if (input.equalsIgnoreCase("stop")) {
+                break; 
+            }
+        int n1 = Integer.parseInt(in.readLine());
+        int n2 = Integer.parseInt(in.readLine());
+        int operazione = Integer.parseInt(in.readLine());
+
+        double ris = 0;
+       
+        switch (operazione) {
+            case 1:
+                ris = n1 + n2;
+                break;
+
+            case 2:
+                ris = n1 - n2;
+                break;
+
+            case 3:
+                ris = n1 / n2;
+                break;
+
+            case 4:
+                ris = n1 * n2;
+                break;
+
+            default:
+                break;
+        }
+     
+          
+        out.println(ris);
+        
+        
+        
+
+        
+
+        mioSocket.close(); 
+    }
+}
+}
